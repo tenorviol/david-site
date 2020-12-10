1177772f84b6e5b0acc8afebb0aa464b97afe4e2
Chris Johnson <tenorviol@yahoo.com>
2020-12-10 08:52:20 -0800

    Use the cat api as an example

--- david-frontend/pages/index.js
+++ david-frontend/pages/index.js
@@ -1,21 +1,48 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
 
-export default function Home() {
+export default function Home({ cat }) {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
+      <main>
+        <h1>
           This is David's Site
         </h1>
 
+        <section>
+          <p>This data was returned by the cat api</p>
+          <code>{JSON.stringify(cat)}</code>
+        </section>
+
+        <section>
+          {cat[0] &&
+            <section>
+              <p>
+                And here's your kitty.
+              </p>
+              <p>
+                <img src={cat[0].url} />
+              </p>
+            </section>
+          }
+        </section>
 
       </main>
     </div>
   )
+}
+
+export async function getStaticProps() {
+  const res = await fetch('https://api.thecatapi.com/v1/images/search')
+  const cat = await res.json()
+
+  return {
+    props: {
+      cat,
+    },
+  }
 }
